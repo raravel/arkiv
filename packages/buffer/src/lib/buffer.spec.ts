d622f1179a0289e35becc51018c98e79801eca14
youn <yjyoon@marketlink.co.kr>
2023-11-10 16:40:59 +0900

    initial commit

---
+++ packages/buffer/src/lib/buffer.spec.ts
@@ -0,0 +1,7 @@
+import { buffer } from './buffer';
+
+describe('buffer', () => {
+  it('should work', () => {
+    expect(buffer()).toEqual('buffer');
+  });
+});
---
+++ packages/buffer/src/lib/buffer.ts
@@ -0,0 +1,3 @@
+export function buffer(): string {
+  return 'buffer';
+}
